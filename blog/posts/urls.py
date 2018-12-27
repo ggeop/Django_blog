8af978331eae36e8d21b0d7e4e038aad6d801097
Georgios Papachristou <35366798+ggeop@users.noreply.github.com>
2018-12-27 12:49:07 +0200

    Instance update view

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -10,8 +10,8 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', post_list),
-    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     url(r'^create/$', post_create),
-    url(r'^update/$', post_update),
+    url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
+    url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
     url(r'^delete/$', post_delete),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
@@ -9,6 +9,8 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
     context = {
        "form": form,
     }
@@ -38,11 +40,20 @@ def post_list(request):
     return render(request, "index.html", context)
 
 
-def post_update(request):
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+
     context = {
-        "title": "Update"
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
     }
-    return render(request, "index.html", context)
+    return render(request, "post_form.html", context)
 
 
 def post_delete(request):
