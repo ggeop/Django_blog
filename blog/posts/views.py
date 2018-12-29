f463a728aeaa3f05328b5124ed124030a9a1173f
Georgios Papachristou <35366798+ggeop@users.noreply.github.com>
2018-12-29 23:40:10 +0200

    Add staff and superuser authentication

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,12 +1,14 @@
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -29,30 +31,27 @@ def post_detail(request, slug=None):
 
 
 def post_list(request):
-    if request.user.is_authenticated():
-        queryset_list = Post.objects.all()
-        paginator = Paginator(queryset_list, 25)  # Show 25 contacts per page
-        page_request_var = 'page'
-        page = request.GET.get(page_request_var)
-        try:
-            queryset = paginator.page(page)
-        except PageNotAnInteger:
-            queryset = paginator.page(1)
-        except EmptyPage:
-            queryset = paginator.page(paginator.num_pages)
-        context = {
-        "title": "Authenticated user List",
-        "object_list": queryset,
-        "page_request_var": page_request_var,
-        }
-    else:
-        context = {
-            "title": "List"
-        }
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 25)  # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        queryset = paginator.page(1)
+    except EmptyPage:
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+    "title": "Authenticated user List",
+    "object_list": queryset,
+    "page_request_var": page_request_var,
+    }
     return render(request, "post_list.html", context)
 
 
 def post_update(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
@@ -69,6 +68,8 @@ def post_update(request, id=None):
 
 
 def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully Deleted")
