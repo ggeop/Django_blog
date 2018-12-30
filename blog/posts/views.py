8bef7c1e35dde25130bb37734df6d0f96e6d1ff6
Georgios Papachristou <35366798+ggeop@users.noreply.github.com>
2018-12-30 02:46:08 +0200

    Add post search functionality

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -41,6 +42,15 @@ def post_list(request):
     queryset_list = Post.objects.active()
     if request.user.is_staff or request.user.is_superuser:
         queryset_list = Post.objects.all()
+
+    query = request.GET.get('q')
+    if query:
+        queryset_list =queryset_list.filter(
+            Q(title__icontains=query)|
+            Q(content__icontains=query)|
+            Q(user__first_name__icontains=query)|
+            Q(user__last_name__icontains=query)
+        ).distinct()
     paginator = Paginator(queryset_list, 25)  # Show 25 contacts per page
     page_request_var = 'page'
     page = request.GET.get(page_request_var)
